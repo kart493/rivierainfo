e84eb80333fe8723e11ef1e40358ba1cbe354c8e
karthik <karthiknadar1204@gmail.com>
2025-02-14 15:43:00 +0530

    script

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Header } from "./components/header";
 import { Footer } from "./components/foter";
 import { RateLimitToast } from "@/components/RateLimitToast";
 import { Toaster } from "@/components/ui/toaster";
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +21,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-R40NJB7Y09"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-R40NJB7Y09');
+        `}
+      </Script>
       <body className={`${inter.className} min-h-screen bg-black text-white`}>
         <Header />
         <main>{children}</main>
-        <GoogleTagManager gtmId="G-R40NJB7Y09" />
         <Toaster />
       </body>
     </html>
